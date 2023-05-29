6603803494b5010a25a565d30f84389cc6206f16
Adithya <adippb@bu.edu>
2023-05-30 01:23:24 +0530

    Added Db Models for Food1Proj

--- Food1Proj/foodPrj1/settings.py
+++ Food1Proj/foodPrj1/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'foods.apps.FoodsConfig'
 ]
 
 MIDDLEWARE = [
@@ -75,8 +76,11 @@ WSGI_APPLICATION = 'foodPrj1.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'calorie_tracker',
+        'USER': 'postgres',
+        'PASSWORD': 'postgr',
+        'HOST': 'localhost'
     }
 }
 
